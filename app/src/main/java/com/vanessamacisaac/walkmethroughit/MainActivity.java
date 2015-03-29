5f4902a2c9dbba1f3d7d2d3c9a324e17a2a19e03
Vanessa MacIsaac <vsmacisaac@gmail.com>
2015-03-29 00:24:11 -0400

    I have never been so happy in my life.
    (the location AND the bluetooth work)

--- app/src/main/java/com/vanessamacisaac/walkmethroughit/MainActivity.java
+++ app/src/main/java/com/vanessamacisaac/walkmethroughit/MainActivity.java
@@ -1,5 +1,9 @@
 package com.vanessamacisaac.walkmethroughit;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.location.Location;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
@@ -35,14 +39,28 @@ import org.w3c.dom.Text;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URL;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 
 public class MainActivity extends ActionBarActivity implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
 
     private static final String TAG = "MainActivity";
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private OutputStream outStream = null;
+
+    // Well known SPP UUID
+    private static UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    // Insert your server's MAC address
+    private static String address = "98:D3:31:40:20:D8";
+
     protected GoogleApiClient mGoogleApiClient;
     protected Location mLastLocation;
 
@@ -129,9 +147,16 @@ public class MainActivity extends ActionBarActivity implements ConnectionCallbac
         mStartUpdatesButton = (Button) findViewById(R.id.start);
         mStopUpdatesButton = (Button) findViewById(R.id.stop);
 
+        //TelephonyManager tManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        //MY_UUID = (UUID) tManager.getDeviceId();
+
+
         // Update values using data stored in the Bundle.
         updateValuesFromBundle(savedInstanceState);
         buildGoogleApiClient();
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        checkBTState();
     }
 
 
@@ -171,14 +196,79 @@ public class MainActivity extends ActionBarActivity implements ConnectionCallbac
     }
 
     @Override
+    public void onPause() {
+        super.onPause();
+
+        Log.d(TAG, "...In onPause()...");
+
+        if (outStream != null) {
+            try {
+                outStream.flush();
+            } catch (IOException e) {
+                errorExit("Fatal Error", "In onPause() and failed to flush output stream: " + e.getMessage() + ".");
+            }
+        }
+
+        try     {
+            btSocket.close();
+        } catch (IOException e2) {
+            errorExit("Fatal Error", "In onPause() and failed to close socket." + e2.getMessage() + ".");
+        }
+    }
+
+
+    @Override
     public void onResume() {
         super.onResume();
         // Within {@code onPause()}, we pause location updates, but leave the
         // connection to GoogleApiClient intact.  Here, we resume receiving
         // location updates if the user has requested them.
 
+        mGoogleApiClient.connect();
+
         if (mGoogleApiClient.isConnected() && mRequestingLocationUpdates) {
+            Log.e(TAG, "*** GOOGLE API CONNECTED && REQUESTING UPDATES");
             startLocationUpdates();
+        }
+
+        // Set up a pointer to the remote node using it's address.
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+        // Two things are needed to make a connection:
+        //   A MAC address, which we got above.
+        //   A Service ID or UUID.  In this case we are using the
+        //     UUID for SPP.
+        try {
+            btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+        } catch (IOException e) {
+            errorExit("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
+        }
+
+        // Discovery is resource intensive.  Make sure it isn't going on
+        // when you attempt to connect and pass your message.
+        btAdapter.cancelDiscovery();
+
+        // Establish the connection.  This will block until it connects.
+        Log.d(TAG, "...Connecting to Remote...");
+        try {
+            btSocket.connect();
+            Log.d(TAG, "...Connection established and data link opened...");
+        } catch (IOException e) {
+            try {
+                btSocket.close();
+            } catch (IOException e2) {
+                errorExit("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
+            }
+        }
+
+        // Create a data stream so we can talk to server.
+        Log.d(TAG, "...Creating Socket...");
+
+        try {
+            outStream = btSocket.getOutputStream();
+            Log.e(TAG, "*** DOES IT REACH HERE ??? ****");
+        } catch (IOException e) {
+            errorExit("Fatal Error", "In onResume() and output stream creation failed:" + e.getMessage() + ".");
         }
     }
 
@@ -186,10 +276,10 @@ public class MainActivity extends ActionBarActivity implements ConnectionCallbac
     @Override
     protected void onStop() {
         super.onStop();
+        stopLocationUpdates();
         if (mGoogleApiClient.isConnected()) {
             mGoogleApiClient.disconnect();
         }
-        stopLocationUpdates();
     }
 
     /**
@@ -488,5 +578,99 @@ public class MainActivity extends ActionBarActivity implements ConnectionCallbac
         }
 
 
+    }
+
+    public void rightBT(View view){
+        checkBTState();
+        sendData("1");
+        Toast msg = Toast.makeText(getBaseContext(),
+                "You have clicked On", Toast.LENGTH_SHORT);
+        msg.show();
+    }
+
+    public void leftBT(View view){
+        checkBTState();
+        sendData("2");
+        Toast msg = Toast.makeText(getBaseContext(),
+                "You have clicked On", Toast.LENGTH_SHORT);
+        msg.show();
+    }
+
+    private void checkBTState() {
+        // Check for Bluetooth support and then check to make sure it is turned on
+
+        // Emulator doesn't support Bluetooth and will return null
+        if(btAdapter==null) {
+            errorExit("Fatal Error", "Bluetooth Not supported. Aborting.");
+        } else {
+            if (btAdapter.isEnabled()) {
+                Log.d(TAG, "...Bluetooth is enabled...");
+            } else {
+                //Prompt user to turn on Bluetooth
+                Intent enableBtIntent = new Intent(btAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
+    }
+
+    private void errorExit(String title, String message){
+        Toast msg = Toast.makeText(getBaseContext(),
+                title + " - " + message, Toast.LENGTH_SHORT);
+        msg.show();
+        finish();
+    }
+
+    private void sendData(String message) {
+        Log.e(TAG, "sendData !!!!");
+        //message = message + "\r";
+        Log.e(TAG, "" + message);
+        byte[] msgBuffer = message.getBytes();
+
+
+        Log.d(TAG, "...Sending data: " + message + "...");
+
+
+        Log.e(TAG, "** CHECK BTSOCKET ** : " + btSocket.isConnected());
+
+        if(!btSocket.isConnected()) {
+            try {
+                btSocket.connect();
+                Thread.sleep(1000);
+                Log.e(TAG, "...Connection established and data link opened...");
+            } catch (IOException e) {
+                try {
+                    btSocket.close();
+                } catch (IOException e2) {
+                    errorExit("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
+                }
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+
+            // Create a data stream so we can talk to server.
+            Log.d(TAG, "...Creating Socket...");
+
+            try {
+                outStream = btSocket.getOutputStream();
+                Log.e(TAG, "*** DOES IT REACH HERE sending ??? ****");
+            } catch (IOException e) {
+                errorExit("Fatal Error", "In onResume() and output stream creation failed:" + e.getMessage() + ".");
+            }
+        }
+        // TRY CATCH BLOCK
+        try {
+            Log.e(TAG, "** ABOUT TO WRITE ???? *** :O *** ");
+            outStream.write(msgBuffer);
+        } catch (IOException e) {
+            String msg = "In onResume() and an exception occurred during write: " + e.getMessage();
+            if (address.equals("00:00:00:00:00:00"))
+                msg = msg + ".\n\nUpdate your server address from 00:00:00:00:00:00 to the correct address on line 37 in the java code";
+            msg = msg + ".\n\nCheck that the SPP UUID: " + MY_UUID.toString() + " exists on server.\n\n";
+
+            errorExit("Fatal Error", msg);
+        }
+        String msg = msgBuffer.toString();
+        Log.e(TAG, "** THE MESSAGE = " + msg);
+        Log.e(TAG, "*** IT SHOULD HAVE WRITTEN HERE");
     }
 }
